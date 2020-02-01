fa64078d21973704a33bdab1c9cc6b8834ebcc08
MarioJim <mario.emilio.j@gmail.com>
2020-02-01 09:57:52 -0600

    Fixed xrandr startup commands

--- scripts/xrandrConfig.sh
+++ scripts/xrandrConfig.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 
-if xrandr | grep "$EDISP connected 2560x1080"; then
+source /home/mario/.zshrc
+
+if xrandr | grep -A 1 "$EDISP connected" | grep "2560x1080"; then
     xrandr --output "$IDISP" --auto --pos 320x1080 --output "$EDISP" --auto --pos 0x0
-elif xrandr | grep "$EDISP connected 1920x1080"; then
+elif xrandr | grep -A 1 "$EDISP connected" | grep "1920x1080"; then
     xrandr --output "$IDISP" --auto --output "$EDISP" --auto --right-of "$IDISP"
 else
     xrandr --output "$IDISP" --auto
